ce4ee61bac1d9b62790fd703a0685504225b4d75
Markus Eckstein <markus.eckstein@gmx.de>
2017-03-10 21:23:21 +0100

    feat: hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 
+(function () {
+    var isOpen = false;
+    var button = document.getElementById('navMenu');
+    var buttonInner = document.getElementById('navMenuInner');
+
+    function toggle() {
+        var className = 'unhide';
+        var aniClassName ='animate';
+        isOpen = !isOpen;
+
+        var navEl = document.getElementById('navMain');
+
+        if (isOpen) {
+            button.classList.add(aniClassName);
+            buttonInner.classList.add(aniClassName);
+            navEl.classList.add(className);
+        } else {
+            button.classList.remove(aniClassName);
+            buttonInner.classList.remove(aniClassName);
+            navEl.classList.remove(className);
+        }
+    };
+
+    
+    button.addEventListener('click', toggle);
+})();
