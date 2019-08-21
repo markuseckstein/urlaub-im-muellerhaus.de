c82111025015573c7137803b95d05a844e4647bc
Markus Eckstein <markus@markuseckstein.de>
2019-08-21 22:30:13 +0200

    more debug output

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,27 +9,28 @@
 const get = require("lodash/get");
 const fs = require("fs");
 const iptc = require("node-iptc");
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     const { createNodeField } = boundActionCreators;
 
     if (node.internal.type === "ImageSharp") {
-        const fileNode = getNode(node.parent)
+        const fileNode = getNode(node.parent);
 
         const fileContent = fs.readFileSync(fileNode.absolutePath);
         const iptcData = iptc(fileContent);
 
+        console.log(
+            "**** iptc data of " + fileNode.absolutePath + ":\n   ",
+            iptcData
+        );
         const title = get(iptcData, ["caption"], "");
 
-        console.log("****  iptc caption of " + fileNode.absolutePath, title);
         createNodeField({
             node,
             name: "file",
             value: {
                 path: fileNode.absolutePath.toString()
             }
-
         });
         createNodeField({
             node,
@@ -38,8 +39,5 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
                 title
             }
         });
-
     }
 };
-
-
